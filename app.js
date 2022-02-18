aa6a92c03512b5e62941d99c474d0e34b3d18c55
Chaithanya45 <y.chaithanya45@gmail.com>
2022-02-18 21:53:00 +0530

    added images

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const request = require("request");
 const https = require("https");
 const ejs = require("ejs");
 const axios = require('axios');
+const fs = require("fs");
 
 const app = express();
 
@@ -18,19 +19,47 @@ app.get("/", function(req, res) {
 })
 
 app.post("/", function(req, res) {
-  const url = "https://movielens-ap.herokuapp.com/movies/content/?movie=" + req.body.movie + "&limit=15"
+  const url = "https://movielens-ap.herokuapp.com/movies/content/?movie=" + req.body.movie + "&limit=10"
   axios
     .get(url)
     .then(response => {
-      var content = response.data.content;
-      res.render("movies", {
-        content: content,
-        title: req.body.movie
+
+      fs.readFile("./data.json", "utf8", (err, jsonString) => {
+        if (err) {
+          console.log("Error reading file from disk:", err);
+          return;
+        }
+        try {
+          const data = JSON.parse(jsonString);
+          var content = response.data.content;
+          const movies = [];
+          for(var k=0;k<data.length;k++){
+            if(req.body.movie===data[k].Movie){
+              movies[0]=data[k];
+              break;
+            }
+          }
+          for(var i=0;i<content.length;i++){
+            for(var j=0;j<data.length;j++){
+              if(content[i]===data[j].Movie){
+                movies[i+1]=data[j];
+                break;
+              }
+            }
+          }
+          res.render("tester", {
+            movies: movies
+          });
+        } catch (err) {
+          console.log("Error parsing JSON string:", err);
+        }
       });
+
     })
     .catch(error => {
       console.log(error.statusCode);
     });
+
 })
 
 
